0f3d5b329bfceca61014c909bab5d8bcada989a7
Renê Germano Júnior <reneagjunior@gmail.com>
2020-01-31 13:08:33 -0300

    crud inicial

--- index.js
+++ index.js
@@ -2,8 +2,41 @@ const express = require("express");
 
 const server = express();
 
-server.get("/", (req, res) => {
-  return res.json({ message: "Hello World!" });
+server.use(express.json());
+
+const projects = [];
+
+server.get("/projects", (req, res) => {
+  return res.json(projects);
+});
+
+server.post("/projects", (req, res) => {
+  const { id, title } = req.body;
+
+  projects.push({ id: id, title: title, tasks: [] });
+
+  return res.json(projects);
+});
+
+server.put("/projects/:id", (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.title = title;
+
+  return res.json(project);
+});
+
+server.delete("/projects/:id", (req, res) => {
+  const { id } = req.params;
+
+  const projectIndex = projects.findIndex(p => p.id == id);
+
+  projects.splice(projectIndex, 1);
+
+  return res.send("Removido com sucesso");
 });
 
 server.listen(3000);
