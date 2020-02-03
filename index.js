484a1819436759cac5e0828a83c899d8d21988f7
Renê Germano Júnior <reneagjunior@gmail.com>
2020-02-03 13:18:39 -0300

    middlewares e inserer tasks

--- index.js
+++ index.js
@@ -6,6 +6,26 @@ server.use(express.json());
 
 const projects = [];
 
+function checkProjectExist(req, res, next) {
+  const { id } = req.params;
+
+  const project = projects.find(p => p.id == id);
+
+  if (!project) {
+    return res.status(400).json({ message: "Não encontrado" });
+  }
+
+  return next();
+}
+
+function logCount(req, res, next) {
+  console.count("Quantidade de requisições");
+
+  return next();
+}
+
+server.use(logCount);
+
 server.get("/projects", (req, res) => {
   return res.json(projects);
 });
@@ -13,12 +33,12 @@ server.get("/projects", (req, res) => {
 server.post("/projects", (req, res) => {
   const { id, title } = req.body;
 
-  projects.push({ id: id, title: title, tasks: [] });
+  projects.push({ id, title, tasks: [] });
 
   return res.json(projects);
 });
 
-server.put("/projects/:id", (req, res) => {
+server.put("/projects/:id", checkProjectExist, (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
 
@@ -29,7 +49,7 @@ server.put("/projects/:id", (req, res) => {
   return res.json(project);
 });
 
-server.delete("/projects/:id", (req, res) => {
+server.delete("/projects/:id", checkProjectExist, (req, res) => {
   const { id } = req.params;
 
   const projectIndex = projects.findIndex(p => p.id == id);
@@ -37,6 +57,17 @@ server.delete("/projects/:id", (req, res) => {
   projects.splice(projectIndex, 1);
 
   return res.send("Removido com sucesso");
+});
+
+server.post("/projects/:id/tasks", checkProjectExist, (req, res) => {
+  const { id } = req.params;
+  const { task } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.tasks.push(task);
+
+  return res.json(project);
 });
 
 server.listen(3000);
